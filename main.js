181e6bae58176ba025a1a6c63f4241ffd5dabc8b
AgusNeira <a2002neira@gmail.com>
2020-12-30 14:04:11 -0300

    [ADD] Client receives device list

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ const SERVER_PORT = process.argv[3] || 3000;
 // Serial port configuration
 const SerialPort = require('serialport');
 const ReadLine = require('@serialport/parser-readline');
+
+// The serial connection establishes after the server, in order to select the port
+/*
 const port = new SerialPort(SERIAL_PORT, { baudRate: 9600 }, err => {
 	if (err)
 		console.log(`No se pudo conectar a ${SERIAL_PORT}:`, err.message);
 	else console.log(`Conexión serial establecida en ${SERIAL_PORT}`);
 });
 const parser = port.pipe(new ReadLine());
+*/
 
 // Server configuration
 const express = require('express');
@@ -30,14 +34,17 @@ app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
 const httpServer = require('http').createServer(app);
-let io = require('socket.io')(httpServer);
 
-httpServer.listen(SERVER_PORT, () => console.log(`Servidor escuchando en localhost:${SERVER_PORT}`));
+httpServer.listen(SERVER_PORT, () => {
+	console.log(`Servidor escuchando en localhost:${SERVER_PORT}`)
+	
+	let io = require('socket.io')(httpServer);
 
-io.on('connect', socket => {
-	console.log('Conexión de sockets establecido');
+	io.on('connect', async socket => {
+		console.log('Conexión de sockets establecido');
 
-	parser.on('data', data => {
-		socket.emit('data-sample', data);
+		// List all available ports and send them to the client
+		let devices = await SerialPort.list();
+		socket.emit('devices', devices);
 	});
 });
